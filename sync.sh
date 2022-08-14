f8d97ccb3d1fe65086b7c6ef0abbf102827cb40f
Nikolay Cherednik <32453685+cherrynik@users.noreply.github.com>
2022-08-14 12:35:17 +0300

    added first plugin

--- sync.sh
+++ sync.sh
@@ -1,1 +1,9 @@
-echo "Hello World!"
+git clone https://github.com/cherrynik/vim.git /tmp/cherrynik-vim-config
+
+cd /tmp/cherrynik-vim-config
+
+cp ./.vimrc ~/
+
+cp -r ./.vim ~/
+
+rm -rf /tmp/cherrynik-vim-config
