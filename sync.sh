2e833ffc2b11a7b80bcd3575725dc7fe6d99df34
Nikolay Cherednik <32453685+cherrynik@users.noreply.github.com>
2022-08-14 16:54:37 +0300

    update

--- sync.sh
+++ sync.sh
@@ -1,8 +1,15 @@
 # General config
+echo ".vimrc cloning..."
 curl https://raw.githubusercontent.com/cherrynik/vim/master/.vimrc > ~/.vimrc
 
 # Plugins
+echo "Plugins cloning..."
+
+echo "* Status bar"
 git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
+
+echo "* Gruvbox Theme"
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/pack/default/start/gruvbox
 
 # cp -r ./.vim/ ~/
 
